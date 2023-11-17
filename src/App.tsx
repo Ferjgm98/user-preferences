3ab6f5b36095ce34f135a6aceb4ae96444757334
Fernando Gonzalez <fernandomenjivar2011@gmail.com>
2023-11-17 08:33:44 -0600

    Display user preference item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { UserPreferences } from "./types/user-preferences.types";
+import UserPreferencesItem from "./components/user-preferences/user-preferences-item.component";
 
 function App() {
 	const [userPreferences, setUserPreferences] = useState<UserPreferences[]>([]);
@@ -15,11 +16,12 @@ function App() {
 		<main className="container">
 			<section className="content">
 				<h1 className="title">User Preferences</h1>
-
-				<div className="user-preferences">
-					{userPreferences.map((item) => (
-						<div>{item.Name}</div>
-					))}
+				<div className="user-preferences-container">
+					<div className="user-preferences">
+						{userPreferences.map((item, idx) => (
+							<UserPreferencesItem key={`${idx}-${item.Name}`} {...item} />
+						))}
+					</div>
 				</div>
 			</section>
 		</main>
---
+++ src/components/user-preferences/user-preferences-item.component.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import {
+	UserPreferences,
+	UserStatusEnum,
+} from "../../types/user-preferences.types";
+import style from "./user-preferences-item.style.module.css";
+
+const UserPreferencesItem: React.FC<UserPreferences> = ({
+	Name,
+	Status,
+	...rest
+}) => {
+	return (
+		<div className={style["user-preferences-wrapper"]}>
+			<h5 className={style["user-preferences-title"]}>{Name}</h5>
+			{Object.keys(rest ?? {}).map((key) => (
+				<p key={key}>
+					{key}: {rest[key as keyof typeof rest]}
+				</p>
+			))}
+			<p
+				className={`${style["user-preferences-status"]} ${
+					Status === UserStatusEnum.Inactive &&
+					style["user-preferences-status-inactive"]
+				}`}
+			>
+				{Status}
+			</p>
+		</div>
+	);
+};
+
+export default UserPreferencesItem;
