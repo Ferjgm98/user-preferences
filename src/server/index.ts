3bafe6561739f33b97acce20f19bb8ca38538fcc
Fernando Gonzalez <fernandomenjivar2011@gmail.com>
2023-11-17 09:26:38 -0600

    Prepare for deployment

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import userPreferencesRouter from "./user-preferences/user-preferences";
+import helmet from "helmet";
+import compression from "compression";
+import path from "path";
 
 const app = express();
+app.use(helmet());
+app.use(compression());
 const port = process.env.PORT || 3002;
 
 app.use(express.json());
@@ -10,7 +15,10 @@ app.use(cors());
 
 app.use("/api/user-preferences", userPreferencesRouter);
 
-app.use(express.static(process.cwd() + "/dist"));
+app.use(express.static(path.join(__dirname, "../")));
+app.get("/*", (_, res) => {
+	res.sendFile(path.join(__dirname, "../", "index.html"));
+});
 app.listen(port, () => {
 	console.log("Server is up and running on port ", port);
 });
